7189c1e33c45ee618960baeea1fdfb73350d7e18
Mike <josemimore0@gmail.com>
2023-07-19 15:27:08 -0400

    styled the projects of the menu

--- src/index.js
+++ src/index.js
@@ -2,21 +2,28 @@ import "./style.scss";
 import Project from "./modules/Project.js";
 import addNewTask from "./modules/addNewTaskToProject";
 import displayTasks from "./ui/displayTasks.js";
-import displayProjects from "./ui/displayProjects.js"
+import displayProjects from "./ui/displayProjects.js";
 import ProjectsManager from "./modules/ProjectsManager";
 
 const work = new Project("Work");
 const home = new Project("Home");
 const school = new Project("School");
+const church = new Project("Church");
+const sideProject = new Project("Side project");
+const travel = new Project("Travel");
 
-export const projectsManager = new ProjectsManager()
+export const projectsManager = new ProjectsManager();
 
-projectsManager.addNewProject(work)
-projectsManager.addNewProject(home)
-projectsManager.addNewProject(school)
+projectsManager.addNewProject(work);
+projectsManager.addNewProject(home);
+projectsManager.addNewProject(school);
+projectsManager.addNewProject(church);
+projectsManager.addNewProject(sideProject);
+projectsManager.addNewProject(travel);
 
+displayProjects(projectsManager.getProjects());
 
-displayProjects(projectsManager.getProjects())
-
-
-displayTasks(projectsManager.getCurrentProject().getTasks(), projectsManager.getCurrentProject().name)
+displayTasks(
+    projectsManager.getCurrentProject().getTasks(),
+    projectsManager.getCurrentProject().name
+);
--- src/ui/displayProjects.js
+++ src/ui/displayProjects.js
@@ -1,19 +1,20 @@
-import changeCurrentProject from "../modules/changeCurrentProject"
+import changeCurrentProject from "../modules/changeCurrentProject";
 
 function createProjectButton(project, index) {
-    let projectButton = document.createElement("button")
-    projectButton.textContent = project.name
-    projectButton.setAttribute("id", index)
-    
-    projectButton.addEventListener("click", changeCurrentProject)
+    let projectButton = document.createElement("button");
+    projectButton.textContent = project.name;
+    projectButton.setAttribute("id", index);
+    projectButton.classList.add("project-button");
 
-    return projectButton
+    projectButton.addEventListener("click", changeCurrentProject);
+
+    return projectButton;
 }
 
 export default function printProjects(projects) {
-    const projectSection = document.getElementById("projects")
+    const projectSection = document.getElementById("projects");
 
     for (let i = 0; i < projects.length; i++) {
-        projectSection.appendChild(createProjectButton(projects[i], i))
+        projectSection.appendChild(createProjectButton(projects[i], i));
     }
 }
